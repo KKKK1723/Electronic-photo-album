2ada12031b0899d91aae3f2bb244ac14db45f379
筏.kkkk <2052436429@qq.com>
2025-04-14 00:49:13 +0800

    实现注册页面浏览按钮功能

--- project-Electronic photo album/prosetpage.cpp
+++ project-Electronic photo album/prosetpage.cpp
@@ -2,16 +2,19 @@
 #include "ui_prosetpage.h"
 #include<QLineEdit>
 #include<QDir>
+#include<QFileDialog>
 ProSetPage::ProSetPage(QWidget *parent)
     : QWizardPage(parent)
     , ui(new Ui::ProSetPage)
 {
     ui->setupUi(this);
-    //注册域 如果没输入 下一步就是灰色的
+    //注册域 如果*字段没输入 下一步就是灰色的
+    registerField("proName*",ui->lineEdit);//*是必填字段
     registerField("proPath",ui->lineEdit_2);
-    registerField("proName*",ui->lineEdit);
+
     connect(ui->lineEdit,&QLineEdit::textEdited,this,&ProSetPage::completeChanged);//触发iscomplete函数
     connect(ui->lineEdit_2,&QLineEdit::textEdited,this,&ProSetPage::completeChanged);
+
     QString curPath=QDir::currentPath();
     ui->lineEdit_2->setText(curPath);//初始的默认路径为当前路径
     //设置光标
@@ -27,13 +30,14 @@ ProSetPage::~ProSetPage()
     delete ui;
 }
 
+//实现强制用户输入正确路径
 bool ProSetPage::isComplete() const
 {
     if(ui->lineEdit->text()==""||ui->lineEdit_2->text()==""){
         return false;//必须要输入
     }
 
-    QDir dir(ui->lineEdit_2->text());//获取文本
+    QDir dir(ui->lineEdit_2->text());//将文本当成路径 判断此路径是否存在
     if(!dir.exists()){
         ui->tips->setText("路径不存在！");
         return false;
@@ -41,13 +45,41 @@ bool ProSetPage::isComplete() const
 
 
     //判断路径
-    QString absFilePath=dir.absoluteFilePath(ui->lineEdit->text());//加上完整路径
+    QString absFilePath=dir.absoluteFilePath(ui->lineEdit->text());//将第一个text也拼接上来形成完整路径
     QDir dist_dir(absFilePath);
     if(dist_dir.exists()){
         ui->tips->setText("项目已存在！请更换路径或名字");
         return false;
     }
 
-    ui->tips->setText("");
+    ui->tips->setText("");//啥也不显示
     return QWizardPage::isComplete();//如果重写了基类的虚函数 后续还想再用其原本的功能 需要返回
 }
+
+//浏览按钮
+void ProSetPage::on_pushButton_clicked()
+{
+    QFileDialog file_dialog;
+    file_dialog.setFileMode(QFileDialog::Directory);//设置为选中文件夹
+    file_dialog.setWindowTitle(tr("选中存放的文件夹"));
+
+    //默认打开路径
+    QDir path=QDir::currentPath();
+    file_dialog.setDirectory(path);//打开当前的文件
+    file_dialog.setViewMode(QFileDialog::Detail);//以详细的信息方式显示
+
+    QStringList list;//存放用户选中的
+    if(file_dialog.exec()){
+        list=file_dialog.selectedFiles();//用户选中的
+    }
+
+    if(list.length()<=0)
+    {
+        return;
+    }
+
+    QString apath=list.at(0);//第一个
+    ui->lineEdit_2->setText(apath);
+
+}
+
