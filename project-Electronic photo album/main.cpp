6e4282e31324f6247976b4deb9ab20e51bea59f6
筏.kkkk <2052436429@qq.com>
2025-04-13 16:46:21 +0800

    Create main.cpp

---
+++ project-Electronic photo album/main.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include<QFile>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    QFile qss(":/style/style.qss");//打开qss文件 加载样式
+    if(qss.open(QFile::ReadOnly))//只读的方式打开
+    {
+        qDebug()<<"open qss success";
+        QString style=QLatin1String(qss.readAll());
+        a.setStyleSheet(style);//设置样式表
+    }
+    else
+    {
+        qDebug()<<"open qss filed";
+        return 0;
+    }
+    w.setWindowTitle("Album");
+    w.showMaximized();//最大化显示
+    return a.exec();
+}
