853f8258b8e7c070cab029e9c0e118afe57c1422
筏.kkkk <2052436429@qq.com>
2025-04-15 10:42:17 +0800

    加入成员变量

--- project-Electronic photo album/protreeitem.h
+++ project-Electronic photo album/protreeitem.h
@@ -9,7 +9,16 @@ public:
 
     //先写上 后面可能用
     ProTreeItem(QTreeWidget* view, const QString& name, const QString& path, int type=Type);
-    ProTreeItem(QTreeWidgetItem* parent,const QString* name, const QString& path,QTreeWidget* root,int type=Type);
+    ProTreeItem(QTreeWidgetItem* parent,const QString& name, const QString& path,QTreeWidgetItem* root,int type=Type);
+
+//成员变量
+private:
+    QString _path;
+    QString _name;
+    QTreeWidgetItem * _root;
+    QTreeWidgetItem * _pre_item;
+    QTreeWidgetItem * _next_item;
+
 };
 
 #endif // PROTREEITEM_H
