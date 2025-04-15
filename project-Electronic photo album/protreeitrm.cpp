93a5c57703962aa65882770af2f43fc40f71ec0c
筏.kkkk <2052436429@qq.com>
2025-04-15 10:41:47 +0800

    初始化

--- project-Electronic photo album/protreeitrm.cpp
+++ project-Electronic photo album/protreeitrm.cpp
@@ -1,11 +1,15 @@
 #include "protreeitem.h"
 
 ProTreeItem::ProTreeItem(QTreeWidget*view,const QString& name,
-                         const QString& path,int type){
+                         const QString& path,int type)
+    :QTreeWidgetItem(view,type),_path(path),_name(name),_root(this),_pre_item(nullptr),_next_item(nullptr)
+{
 
 }
 
-ProTreeItem::ProTreeItem(QTreeWidgetItem* parent,const QString*name,const QString& path,
-                         QTreeWidget*root,int type){
+ProTreeItem:: ProTreeItem(QTreeWidgetItem* parent,const QString& name, const QString& path,QTreeWidgetItem* root,int type)
+    :QTreeWidgetItem(parent,type),
+    _path(path),_name(name),_root(root),_pre_item(nullptr),_next_item(nullptr)
+{
 
 }
