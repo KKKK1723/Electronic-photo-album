b639ff58539909152de517498b4265da983cd8f1
筏.kkkk <2052436429@qq.com>
2025-04-18 01:28:01 +0800

    Update protreewidget.cpp

--- project-Electronic photo album/protreewidget.cpp
+++ project-Electronic photo album/protreewidget.cpp
@@ -5,43 +5,49 @@
 #include<QGuiApplication>
 #include<QMenu>
 #include<QFileDialog>
+#include"protreethread.h"
+
 ProTreeWidget::ProTreeWidget(QWidget *parent) {
     this->setHeaderHidden(true);//把数字隐藏掉
-    connect(this,&ProTreeWidget::itemPressed,this,&ProTreeWidget::SlotItemPress);
+    connect(this,&ProTreeWidget::itemPressed,this,&ProTreeWidget::SlotItemPress);//右键根目录打开菜单
     _action_import=new QAction(QIcon("qrc:/photo/1daeb62f494e3a0f040ea34c18cd41f.jpg"),tr("导入文件"),this);//this绑定父窗口 就不需要手动回收了
     _action_setstart=new QAction(QIcon("qrc:/photo/1daeb62f494e3a0f040ea34c18cd41f.jpg"),tr("设置活动相册"),this);
     _action_closerpro=new QAction(QIcon("qrc:/photo/1daeb62f494e3a0f040ea34c18cd41f.jpg"),tr("关闭相册"),this);
     _action_slideshow=new QAction(QIcon("qrc:/photo/1daeb62f494e3a0f040ea34c18cd41f.jpg"),tr("轮播图播放"),this);
 
-    connect(_action_import,&QAction::triggered,this,&ProTreeWidget::SlotImport);
+    connect(_action_import,&QAction::triggered,this,&ProTreeWidget::SlotImport);//点击导入文件后执行SlotImport函数
 
 }
 
-void ProTreeWidget::AddProTree(const QString &name, const QString &path)
+
+void ProTreeWidget::AddProTree(const QString &name, const QString &path)//创建相册到目录中
 {
-    QDir dir(path);//指向这个路径
-    QString file_path=dir.absoluteFilePath(name);//加上前面的名字 拼接起一个完整的路径
-    if(_set_path.find(file_path)!=_set_path.end()){//找到了  表示重名了
+    QDir dir(path);
+    QString _path=dir.absoluteFilePath(name);
+    if(_set_path.find(_path)!=_set_path.end())//有这个路径了
+    {
         return;
     }
 
-    QDir pro_dir(file_path);//用这个路径创建一个文件夹  看此文件夹是否存在
-    if(!pro_dir.exists()){
-        bool enable=pro_dir.mkpath(file_path);
-        if(!enable){//不成功
-            return;
+    QDir p_dir(_path);
+    if(!p_dir.exists())//不存在就创建
+    {
+        bool p=p_dir.mkpath(_path);
+        if(!p)
+        {
+            return;//创建失败
         }
     }
 
-    _set_path.insert(file_path);
-    auto * item=new ProTreeItem(this,name,file_path,TreeItemPro);
+    _set_path.insert(_path);
+    auto item=new ProTreeItem(this,name,_path,TreeItemPro);
     item->setData(0,Qt::DisplayRole,name);
     item->setData(0,Qt::DecorationRole,QIcon(":/photo/e94dcc6cc2a06e03229c65a6d1a7e70.jpg"));
-    item->setData(0,Qt::ToolTipRole,file_path);//鼠标悬浮时提示文件路径
-    this->addTopLevelItem(item);//加进来
+    item->setData(0,Qt::ToolTipRole,_path);
+    this->addTopLevelItem(item);
 }
 
-void ProTreeWidget::SlotItemPress(QTreeWidgetItem *item, int column)
+void ProTreeWidget::SlotItemPress(QTreeWidgetItem *item, int column)//右键打开菜单
 {
     if(QGuiApplication::mouseButtons()&Qt::RightButton)//右键点击
     {
@@ -60,11 +66,37 @@ void ProTreeWidget::SlotItemPress(QTreeWidgetItem *item, int column)
     }
 }
 
-void ProTreeWidget::SlotImport()
+void ProTreeWidget::SlotImport()//菜单选项一  导入文件
 {
-    QFileDialog file_dialog;//创建一个文件对话框对象 file_dialog，用于打开或保存文件
-    file_dialog.setFileMode(QFileDialog::Directory);//打开文件夹
-    file_dialog.setWindowTitle(tr("选择导入的文件夹"));
-    QString path="";
+    QFileDialog file_log;
+    file_log.setFileMode(QFileDialog::Directory);//文件夹模式
+    file_log.setViewMode(QFileDialog::Detail);//详细模式
+    file_log.setWindowTitle(tr("选择导入文件夹"));
+    QString file_log_path=dynamic_cast<ProTreeItem*>(_right_btn_item)->GetPath();
+    file_log.setDirectory(file_log_path);//打开路径
+
+    QStringList _list;
+    if(file_log.exec())
+    {
+        _list=file_log.selectedFiles();//获取用户选中的文件然后存储在_list中
+    }
+
+    if(_list.length()<=0)//用户没有选择任何文件
+    {
+        return;
+    }
+
+    QString first_path=_list.at(0);
+    int file_count=0;//记录文件中图片有多少个
+
+    _thread_create_p =std::make_shared<ProTreeThread>(std::ref(first_path),std::ref(file_log_path),_right_btn_item,file_count,this,_right_btn_item,nullptr);
+    _thread_create_p->start();
+    //主页面创建一个模态对话框显示加载进度
+    _dialog_progress=new QProgressDialog(this);
+    //给对话框初始化
+    _dialog_progress->setWindowTitle(tr("Please wait..."));
+    _dialog_progress->setFixedWidth(PROGRESS_WIDTH);
+    _dialog_progress->setRange(0,PROGRESS_WIDTH);
+    _dialog_progress->exec();
 
 }
