4e64f57ad4044b70e465309b0e49ea13f7097da9
筏.kkkk <2052436429@qq.com>
2025-04-18 17:07:52 +0800

    Update protreeitem.h

--- project-Electronic photo album/protreeitem.h
+++ project-Electronic photo album/protreeitem.h
@@ -7,17 +7,26 @@ class ProTreeItem : public QTreeWidgetItem
 {
 public:
 
-    //先写上 后面可能用
-    ProTreeItem(QTreeWidget* view, const QString& name, const QString& path, int type=Type);
-    ProTreeItem(QTreeWidgetItem* parent,const QString& name, const QString& path,QTreeWidgetItem* root,int type=Type);
+    //构造函数
+    ProTreeItem(QTreeWidget* view, const QString& name, const QString& path, int type=Type);//创建顶层节点
+    ProTreeItem(QTreeWidgetItem* parent,const QString& name, const QString& path,QTreeWidgetItem* root,int type=Type);//给某个节点添加子项
+
+    const QString& GetPath();//获取路径
+    QTreeWidgetItem* GetRoot();//获取根目录
+    void SetPreItem(QTreeWidgetItem* item);//前
+    void SetNextItem(QTreeWidgetItem* item);//后
+    ProTreeItem* GetPreItem();//前一个
+    ProTreeItem* GetNextItem();//下一个
+    ProTreeItem* GetLastPicChild();
+    ProTreeItem* GetFirstPicChild();
 
 //成员变量
 private:
-    QString _path;
-    QString _name;
-    QTreeWidgetItem * _root;
-    QTreeWidgetItem * _pre_item;
-    QTreeWidgetItem * _next_item;
+    QString _path;//当前节点路径
+    QString _name;//当前节点名称
+    QTreeWidgetItem * _root;//当前节点的根目录
+    QTreeWidgetItem * _pre_item;//前一个节点
+    QTreeWidgetItem * _next_item;//后一个节点
 
 };
 
