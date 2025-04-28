39373590c2a90023f61aa0e9b4600a36bd9f6e26
筏.kkkk <2052436429@qq.com>
2025-04-28 09:40:15 +0800

    Update const.h

--- const.h
+++ const.h
@@ -9,6 +9,18 @@ enum TressItemType{
     TreeItemPic=3,//表示项目的图片
 };
 
+//播放状态 静止状态
+enum PicBtnState{
+    PicBtnStateNormal=1,
+    PicBtnStateHover=2,
+    PicBtnStatePress=3,
+
+    PicBtnStateNormal2=4,
+    PicBtnStateHover2=5,
+    PicBtnStatePress2=6,
+};
 
 const int PROGRESS_WIDTH = 300;
 const int PROGRESS_MAX=300;
+const int PREITEM_SIZE=100;
+const int PREICON_SIZE=90;
