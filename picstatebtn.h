0451706312ed899bcd9e17d31aaf7d0eb7b3c8b4
筏.kkkk <2052436429@qq.com>
2025-04-28 09:46:40 +0800

    Create picstatebtn.h

---
+++ picstatebtn.h
@@ -0,0 +1,35 @@
+#ifndef PICSTATEBTN_H
+#define PICSTATEBTN_H
+
+#include <QPushButton>
+
+class PicStateBtn : public QPushButton
+{
+    Q_OBJECT
+public:
+    PicStateBtn(QWidget *parent = nullptr);
+    void SetIcon(const QString& normal,const QString& hover,const QString& pressed,//正常1 悬浮1 点击1 时的按钮图片样式
+                 const QString& normal2,const QString& hover2,const QString& pressed2);//正常2 悬浮2 点击2 时的按钮图片样式
+
+protected:
+    bool event(QEvent *e) override;
+
+private:
+    void setNormalIcon();
+    void setHoverIcon();
+    void setPressIcon();
+
+    void setNormalIcon2();
+    void setHoverIcon2();
+    void setPressIcon2();
+
+    QString _normal;
+    QString _hover;
+    QString _pressed;
+    QString _normal2;
+    QString _hover2;
+    QString _pressed2;
+    int _cur_state;
+};
+
+#endif // PICSTATEBTN_H
