06f1ee0499904630915fbab40dbc5b97360d3ea0
筏.kkkk <2052436429@qq.com>
2025-04-28 09:43:18 +0800

    Update protreeitem.cpp

--- protreeitem.cpp
+++ protreeitem.cpp
@@ -1,5 +1,5 @@
 #include "protreeitem.h"
-
+#include"const.h"
 ProTreeItem::ProTreeItem(QTreeWidget*view,const QString& name,
                          const QString& path,int type)
     :QTreeWidgetItem(view,type),_path(path),_name(name),_root(this),_pre_item(nullptr),_next_item(nullptr)
@@ -46,4 +46,71 @@ ProTreeItem *ProTreeItem::GetPreItem()
 ProTreeItem *ProTreeItem::GetNextItem()
 {
     return dynamic_cast<ProTreeItem*>(_next_item);//从父类转为子类
+}
+
+ProTreeItem *ProTreeItem::GetLastPicChild()//获取最后一个子节点
+{
+    if(this->type()==TreeItemPic)
+    {
+        return nullptr;
+    }
+
+    auto child_count=this->childCount();
+    if(child_count==0)
+    {
+        return nullptr;
+    }
+
+    for(int i=child_count-1;i>=0;i--)
+    {
+        auto * last_child=this->child(i);
+        auto * last_tree_child=dynamic_cast<ProTreeItem*>(last_child);
+        if(last_tree_child->type()==TreeItemPic)//是图片
+        {
+            return last_tree_child;
+        }
+
+        //是文件夹
+        last_child=last_tree_child->GetLastPicChild();
+        if(!last_child)continue;
+
+        last_tree_child=dynamic_cast<ProTreeItem*>(last_child);
+        return last_tree_child;
+
+    }
+    return nullptr;
+
+}
+
+ProTreeItem *ProTreeItem::GetFirstPicChild()
+{
+    if(this->type()==TreeItemPic)
+    {
+        return nullptr;
+    }
+
+    auto child_count=this->childCount();
+    if(child_count==0)
+    {
+        return nullptr;
+    }
+
+    for(int i=0;i<child_count-1;i++)
+    {
+        auto * first_child=this->child(i);
+        auto * first_tree_child=dynamic_cast<ProTreeItem*>(first_child);
+        if(first_tree_child->type()==TreeItemPic)//是图片
+        {
+            return first_tree_child;
+        }
+
+        //是文件夹
+        first_child=first_tree_child->GetFirstPicChild();
+        if(!first_child)continue;
+
+        first_tree_child=dynamic_cast<ProTreeItem*>(first_child);
+        return first_tree_child;
+
+    }
+    return nullptr;
 }
